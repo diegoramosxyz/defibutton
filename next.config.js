e6bec5bed46cb4b42b1342d6392c5bbd0082232a
Diego Ramos <RamosDiego@pm.me>
2021-03-28 14:54:46 -0400

    video test

--- next.config.js
+++ next.config.js
@@ -2,4 +2,21 @@ const { i18n } = require('./next-i18next.config')
 
 module.exports = {
   i18n,
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+        ],
+      },
+    ]
+  },
 }
