32ba4386f1dd5e4f9d4b7a5735eb01fdee8b730d
Diego Ramos <ramosdiego@pm.me>
2021-12-05 11:43:44 -0500

    removed goatcounter

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,12 +16,6 @@ class MyDocument extends Document {
     return (
       <Html className="bg-trueGray-50 text-trueGray-800 dark:bg-trueGray-900 dark:text-trueGray-200">
         <Head />
-        {/* Count adjusted for Next.js Link behaviour */}
-        {/* https://github.com/zgoat/goatcounter/issues/482 */}
-        <script async src="/goatcounter-custom-count.js"></script>
-        {/* GOATCOUNTER ANALYTICS */}
-        <script data-goatcounter="https://stats.defibutton.com/count"
-        async src="//stats.defibutton.com/count.js"></script>
         {/* Must  */}
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
--- public/goatcounter-custom-count.js
+++
@@ -1,9 +0,0 @@
-(function (history) {
-  var pushState = history.pushState
-  history.pushState = function (state) {
-    window.goatcounter.count({
-        path: location.pathname,
-      })
-    return pushState.apply(history, arguments)
-  }
-})(window.history)
