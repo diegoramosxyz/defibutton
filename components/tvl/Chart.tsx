8586d74cba4a80394f53b95296476080e619df2d
Diego Ramos <ramosdiego@pm.me>
2021-12-09 23:02:43 -0500

    updated chart

--- components/tvl/Chart.tsx
+++ components/tvl/Chart.tsx
@@ -1,6 +1,26 @@
 import React, { useEffect, useState } from 'react'
 import { Line } from 'react-chartjs-2'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js'
 import { RoundBigNumber } from 'utils/numbers'
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+)
 
 export default function LineChart() {
   const [apiTvls, setApiTvls] = useState([])
@@ -44,60 +64,57 @@ export default function LineChart() {
       </h1>
       <Line
         data={data}
-        options={options}
-        legend={{ labels: { usePointStyle: true } }}
+        options={{
+          responsive: true,
+
+          elements: {
+            point: {
+              radius: 0,
+            },
+          },
+          interaction: {
+            mode: 'index',
+            intersect: false,
+          },
+          scales: {
+            x: {
+              // gridLines: {
+              //   display: false,
+              // },
+              ticks: {
+                // autoskip: true,
+                autoSkipPadding: 50,
+              },
+              // type: 'time',
+              // distribution: 'series',
+              time: {
+                unit: 'day',
+                displayFormats: {
+                  day: 'MMM D',
+                },
+                tooltipFormat: 'MMM D, YYYY',
+              },
+            },
+            y: {
+              // gridLines: {
+              //   display: false,
+              // },
+              // ticks: {
+              //   beginAtZero: false,
+              // },
+            },
+          },
+          plugins: {
+            legend: {
+              position: 'top' as const,
+            },
+            title: {
+              display: true,
+              text: 'Chart.js Line Chart',
+            },
+          },
+        }}
       />
     </section>
   )
-}
-
-const options = {
-  animation: {
-    duration: 0,
-  },
-  elements: {
-    point: {
-      radius: 0,
-    },
-  },
-  tooltips: {
-    // mode: 'nearest',
-    intersect: false,
-  },
-  // responsive: true,
-  // maintainAspectRatio: true,
-  scales: {
-    xAxes: [
-      {
-        gridLines: {
-          display: false,
-        },
-        ticks: {
-          autoskip: true,
-          autoSkipPadding: 50,
-        },
-        type: 'time',
-        distribution: 'series',
-        time: {
-          unit: 'day',
-          displayFormats: {
-            day: 'MMM D',
-          },
-          tooltipFormat: 'MMM D, YYYY',
-        },
-      },
-    ],
-    yAxes: [
-      {
-        gridLines: {
-          display: false,
-          // drawOnChartArea: true,
-        },
-        ticks: {
-          // stepSize: 1,
-          beginAtZero: false,
-        },
-      },
-    ],
-  },
 }
--- components/tvl/Chart2.tsx
+++ components/tvl/Chart2.tsx
@@ -1,7 +1,27 @@
 import { ProtocolTvl } from 'interfaces'
 import React from 'react'
 import { Line } from 'react-chartjs-2'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js'
 import { RoundBigNumber } from 'utils/numbers'
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+)
 
 export default function LineChart({ tvl }: { tvl: ProtocolTvl['tvl'] }) {
   // Format dates because the API doesn't provide a good format.
@@ -35,60 +55,57 @@ export default function LineChart({ tvl }: { tvl: ProtocolTvl['tvl'] }) {
       </h1>
       <Line
         data={data}
-        options={options}
-        legend={{ labels: { usePointStyle: true } }}
+        options={{
+          responsive: true,
+
+          elements: {
+            point: {
+              radius: 0,
+            },
+          },
+          interaction: {
+            mode: 'index',
+            intersect: false,
+          },
+          scales: {
+            x: {
+              // gridLines: {
+              //   display: false,
+              // },
+              ticks: {
+                // autoskip: true,
+                autoSkipPadding: 50,
+              },
+              // type: 'time',
+              // distribution: 'series',
+              time: {
+                unit: 'day',
+                displayFormats: {
+                  day: 'MMM D',
+                },
+                tooltipFormat: 'MMM D, YYYY',
+              },
+            },
+            y: {
+              // gridLines: {
+              //   display: false,
+              // },
+              // ticks: {
+              //   beginAtZero: false,
+              // },
+            },
+          },
+          plugins: {
+            legend: {
+              position: 'top' as const,
+            },
+            title: {
+              display: true,
+              text: 'Chart.js Line Chart',
+            },
+          },
+        }}
       />
     </section>
   )
-}
-
-const options = {
-  animation: {
-    duration: 0,
-  },
-  elements: {
-    point: {
-      radius: 0,
-    },
-  },
-  tooltips: {
-    // mode: 'nearest',
-    intersect: false,
-  },
-  // responsive: true,
-  // maintainAspectRatio: true,
-  scales: {
-    xAxes: [
-      {
-        gridLines: {
-          display: false,
-        },
-        ticks: {
-          autoskip: true,
-          autoSkipPadding: 50,
-        },
-        type: 'time',
-        distribution: 'series',
-        time: {
-          unit: 'day',
-          displayFormats: {
-            day: 'MMM D',
-          },
-          tooltipFormat: 'MMM D, YYYY',
-        },
-      },
-    ],
-    yAxes: [
-      {
-        gridLines: {
-          display: false,
-          // drawOnChartArea: true,
-        },
-        ticks: {
-          // stepSize: 1,
-          beginAtZero: false,
-        },
-      },
-    ],
-  },
 }
